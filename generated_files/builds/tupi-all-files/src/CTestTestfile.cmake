b9fe7b4dfff4fe0a6156b6a66a5a968092355e2f
datacenter <jessejusoli@gmail.com>
2017-12-31 08:12:26 +0000

    init

---
+++ generated_files/builds/tupi-all-files/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/workspace/generated_files/tupi/src
+# Build directory: /home/ubuntu/workspace/generated_files/tupi/build/release/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ generated_files/tupi/src/P2p/P2pNetworks.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { {  0xc2, 0xc0, 0x37, 0xd1, 0x7b, 0x93, 0xb6, 0x54, 0x62, 0x44, 0x30, 0x6f, 0x7a, 0x75, 0x37, 0xf3  } };
+
+}
+
